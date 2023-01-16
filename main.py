b31c1cb267490d56684ae54aa17ac77b118592f2
kkjb <9390833+kkjb@users.noreply.github.com>
2023-01-16 21:29:49 +0800

    Update main.py
    Description add for sys.argv

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 import fnmatch
 import sys
 
-def get_video_info(filepath):
+def get_mp4_video_info(filepath):
 
     probe = ffmpeg.probe(filepath)
     
@@ -49,6 +49,8 @@ def filter_mp4_video(video_path):
 
 ### two input folder path 
 
+
+# You can compile this code into exe by pyinstaller 
 if __name__ == '__main__':
 
     video_info_list =[ ]
@@ -61,6 +63,29 @@ if __name__ == '__main__':
         "height": "1080",
     }
 
+
+
+# using sys.argv to acquire the needed arguments
+
+# after compiled into exe 
+# the sys.argv will be the following
+# print (os.getcwd())     ---->>      C:\\Users\\name\\SourceCode                                           ---->> The folder where you place the source code
+# print(sys.path[0])      ---->>      C:\\Users\\name\\AppData\\Local\\Temp\\_MEI111922\\base_library.zip   ---->> Compiled exe runtime temporary folder
+# print(sys.argv[0])      ---->>      C:\\Users\\name\\SourceCode\\compiled\\dist\\main.exe                 ---->> The folder where the compiled exe is located
+
+# 因此sys.argv[0]为程序本身所在目录
+
+# video_path is the global variable used to store the input folder path 
+
+# if you input a video file path like this form "C:\\Users\\name\\sample.mp4"
+# you can use os.path.split(File_name) to split it two dirname and basename
+# path_tuple = os.path.split(path)
+# dirname    = path_tuple[0]
+# basename   = path_tuple[1]
+
+# The code is currently not accepting file path, please drag the folder into the terminal
+
+
     if len(sys.argv) != 2:
         video_path = input('请拖入视频文件夹,并回车:\n')
             
@@ -72,7 +97,7 @@ if __name__ == '__main__':
             mp4_list = filter_mp4_video(video_path.strip('"'))
 
             for i in range(len(mp4_list)):
-                get_video_info(mp4_list[i])
+                get_mp4_video_info(mp4_list[i])
                 print(video_info_list[i])
              
     else:
